bd70c59c3b50946255febf3b23f0e2bf0fee0ac7
Amereth <alek.melnyk@gmail.com>
2024-10-19 23:22:39 +0300

    created minSizedInput component

---
+++ src/lib/components/minSizeInput.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	const {
+		value: _value,
+		name,
+		className,
+	}: { value?: number; name: string; className?: string } = $props()
+
+	let value = $state(_value)
+</script>
+
+<input
+	bind:value
+	{name}
+	class={className}
+	style:width={value?.toString().length + 'ch'}
+	type="text"
+/>
