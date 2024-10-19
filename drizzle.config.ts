b1236b73b1e5e27d6a1f4499b03ed6d0f3bed31b
Amereth <alek.melnyk@gmail.com>
2024-10-19 16:32:57 +0300

    fixed drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,8 +2,7 @@ import { defineConfig } from 'drizzle-kit'
 
 export default defineConfig({
 	schema: './src/lib/server/schemas/index.ts',
-	dialect: 'sqlite',
-	driver: 'turso',
+	dialect: 'turso',
 	dbCredentials: {
 		url: process.env.DATABASE_URL as string,
 		authToken: process.env.DATABASE_AUTH_TOKEN as string,
