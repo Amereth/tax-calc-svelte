ed02651bd5e46368c2dbe8292a1b0ab1489bb6f7
Amereth <alek.melnyk@gmail.com>
2024-12-11 15:41:19 +0200

    added skeleton ui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,11 @@
 import type { Config } from 'tailwindcss'
 import daisyui from 'daisyui'
+import { skeleton } from '@skeletonlabs/tw-plugin'
 
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
 		extend: {},
 	},
-	plugins: [daisyui],
-} as Config
+	plugins: [daisyui, skeleton],
+} satisfies Config as Config
