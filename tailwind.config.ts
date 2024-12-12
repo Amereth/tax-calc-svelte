26266402ae2460d03ef6020fabca21e02d1d24c6
Amereth <alek.melnyk@gmail.com>
2024-12-12 22:11:17 +0200

    theme refactoring

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,27 +1,58 @@
 import type { Config } from 'tailwindcss'
 import daisyui from 'daisyui'
 import { skeleton } from '@skeletonlabs/tw-plugin'
-import { mainTheme } from './src/configs/theme'
+import { mainTheme, colors } from './src/configs/theme'
 import forms from '@tailwindcss/forms'
 
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: {
+					DEFAULT: colors.primary[500],
+					...colors.primary,
+				},
+				secondary: {
+					DEFAULT: colors.secondary[500],
+					...colors.secondary,
+				},
+				tertiary: {
+					DEFAULT: colors.tertiary[500],
+					...colors.tertiary,
+				},
+				success: {
+					DEFAULT: colors.success[500],
+					...colors.success,
+				},
+				warning: {
+					DEFAULT: colors.warning[500],
+					...colors.warning,
+				},
+				error: {
+					DEFAULT: colors.error[500],
+					...colors.error,
+				},
+				surface: {
+					DEFAULT: colors.surface[500],
+					...colors.surface,
+				},
+			},
+		},
 	},
 	daisyui: {
 		prefix: 'd-',
 		themes: [
 			{
 				mainTheme: {
-					primary: '#84bdf5',
-					secondary: '#d36494',
-					accent: '#aebbc0',
-					neutral: '#371d3a',
-					info: '#aebbc0',
-					success: '#6fc885',
-					warning: '#fcfa19',
-					error: '#ff3838',
+					primary: colors.primary[500],
+					secondary: colors.secondary[500],
+					accent: colors.tertiary[500],
+					neutral: colors.surface[500],
+					info: colors.primary[500],
+					success: colors.success[500],
+					warning: colors.warning[500],
+					error: colors.error[500],
 				},
 			},
 		],
