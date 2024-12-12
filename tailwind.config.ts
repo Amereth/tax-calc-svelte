ee0cc41c34fa9f4cd94e023228203a628470ab0b
Amereth <alek.melnyk@gmail.com>
2024-12-12 22:33:27 +0200

    removed tailwind default colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,36 +7,34 @@ import forms from '@tailwindcss/forms'
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {
-			colors: {
-				primary: {
-					DEFAULT: colors.primary[500],
-					...colors.primary,
-				},
-				secondary: {
-					DEFAULT: colors.secondary[500],
-					...colors.secondary,
-				},
-				tertiary: {
-					DEFAULT: colors.tertiary[500],
-					...colors.tertiary,
-				},
-				success: {
-					DEFAULT: colors.success[500],
-					...colors.success,
-				},
-				warning: {
-					DEFAULT: colors.warning[500],
-					...colors.warning,
-				},
-				error: {
-					DEFAULT: colors.error[500],
-					...colors.error,
-				},
-				surface: {
-					DEFAULT: colors.surface[500],
-					...colors.surface,
-				},
+		colors: {
+			primary: {
+				DEFAULT: colors.primary[500],
+				...colors.primary,
+			},
+			secondary: {
+				DEFAULT: colors.secondary[500],
+				...colors.secondary,
+			},
+			tertiary: {
+				DEFAULT: colors.tertiary[500],
+				...colors.tertiary,
+			},
+			success: {
+				DEFAULT: colors.success[500],
+				...colors.success,
+			},
+			warning: {
+				DEFAULT: colors.warning[500],
+				...colors.warning,
+			},
+			error: {
+				DEFAULT: colors.error[500],
+				...colors.error,
+			},
+			surface: {
+				DEFAULT: colors.surface[500],
+				...colors.surface,
 			},
 		},
 	},
