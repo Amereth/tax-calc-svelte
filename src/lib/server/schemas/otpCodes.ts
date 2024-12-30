f8ad718cfc88ef911b5d0012b312a341bd08318d
amereth <alek.melnyk@gmail.com>
2024-12-31 00:30:00 +0200

    dropped column isUsed on table otpCodes

--- src/lib/server/schemas/otpCodes.ts
+++ src/lib/server/schemas/otpCodes.ts
@@ -12,7 +12,6 @@ export const otpCodes = sqliteTable('otp_codes', {
 		.notNull()
 		.references(() => users.email),
 	expiryDate: integer('expiry_date').notNull(),
-	isUsed: integer('is_used').notNull().default(0),
 })
 
 export type OtpCode = InferSelectModel<typeof otpCodes>
