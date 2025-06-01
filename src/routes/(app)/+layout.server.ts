fb24211a29e865c7b3a83b29c6bb1ffdaac0c914
oleksandr-iagon <oleksandr@iagon.com>
2025-06-01 21:14:20 +0300

    add current year

--- src/routes/(app)/+layout.server.ts
+++ src/routes/(app)/+layout.server.ts
@@ -4,5 +4,9 @@ import type { LayoutServerLoad } from './$types'
 export const load: LayoutServerLoad = async () => {
 	const years = await getYears()
 
+	if (!years.includes(new Date().getFullYear().toString())) {
+		years.push(new Date().getFullYear().toString())
+	}
+
 	return { years }
 }
