a5b4e060851acd5f757def74d971d11422c22547
oleksandr-iagon <oleksandr@iagon.com>
2025-07-20 22:01:26 +0300

    removed daisy ui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,4 @@
 import type { Config } from 'tailwindcss'
-import daisyui from 'daisyui'
 import { skeleton } from '@skeletonlabs/tw-plugin'
 import { mainTheme, colors } from './src/configs/theme'
 import forms from '@tailwindcss/forms'
@@ -40,27 +39,9 @@ export default {
 			},
 		},
 	},
-	daisyui: {
-		prefix: 'd-',
-		themes: [
-			{
-				mainTheme: {
-					primary: colors.primary[500],
-					secondary: colors.secondary[500],
-					accent: colors.tertiary[500],
-					neutral: colors.surface[500],
-					info: colors.primary[500],
-					success: colors.success[500],
-					warning: colors.warning[500],
-					error: colors.error[500],
-				},
-			},
-		],
-	},
 	darkMode: 'class',
 	plugins: [
 		forms,
-		daisyui,
 		skeleton({
 			themes: {
 				custom: [mainTheme],
