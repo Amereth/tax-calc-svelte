fc3a5888f5ceb0947ffcb9d598f4d2bb4002606b
oleksandr-iagon <oleksandr@iagon.com>
2025-07-27 01:25:41 +0300

    fix get taxes

--- src/lib/server/utils/getTaxes.ts
+++ src/lib/server/utils/getTaxes.ts
@@ -1,13 +1,19 @@
-import { and, between } from 'drizzle-orm'
+import { and, between, eq } from 'drizzle-orm'
 import { db } from '../db'
 import { taxes } from '../schemas/tax'
 
-export const getTaxes = ({ year }: { year?: number } = {}) =>
+export const getTaxes = ({
+	year,
+	name,
+}: { year?: number; name?: string } = {}) =>
 	db
 		.select()
 		.from(taxes)
 		.where(
-			year
-				? and(between(taxes.date, `${year}-01-01`, `${year}-12-31`))
-				: undefined,
+			and(
+				year
+					? and(between(taxes.date, `${year}-01-01`, `${year}-12-31`))
+					: undefined,
+				name ? eq(taxes.name, name) : undefined,
+			),
 		)
