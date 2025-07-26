109fd9408e08831e2fd75ce7b9f84a80a53ff0e7
oleksandr-iagon <oleksandr@iagon.com>
2025-07-27 01:25:50 +0300

    fix no value

--- src/lib/utils/getCurrentYearValues.ts
+++ src/lib/utils/getCurrentYearValues.ts
@@ -2,7 +2,7 @@ import type { Tax, Payment } from '$lib/server/schemas/types'
 
 export const getCurrentYearValues = <T extends Payment | Tax>(
 	year: string,
-	payments: T[],
+	payments: T[] = [],
 ): T[] =>
 	payments.filter((payment) => {
 		const [paymentYear] = payment.date.split('-').map(Number)
